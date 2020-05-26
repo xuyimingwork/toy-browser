74cd5196aa01a8659f10371edb933ec60a6e8185
徐一鸣#80941041 <80941041@yonghui.cn>
2020-05-26 20:02:10 +0800

    feat(client): setup client and try net.createConnection

---
+++ client.js
@@ -0,0 +1,26 @@
+const net = require('net');
+
+class Request {
+
+}
+
+class Response {
+
+}
+
+const client = net.createConnection({ 
+  host: '127.0.0.1',
+  port: 8088 }, () => {
+  console.log('连接成功，发送数据');
+  client.write('world!\r\n');
+});
+
+client.on('data', (data) => {
+  console.log('接收返回数据');
+  console.log(data.toString());
+  client.end();
+});
+
+client.on('end', () => {
+  console.log('断开连接');
+});
