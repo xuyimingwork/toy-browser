c8fe89cf92162b25142fd635b746cd4b4ffb9975
xuyiming <xuyimingwork@foxmail.com>
2020-05-28 15:57:07 +0800

    feat(server): support read and response html file

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   console.log(`${req.method} ${req.url} HTTP/${req.httpVersion}`);
@@ -6,7 +7,8 @@ const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('X-Foo', 'bar');
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('ok');
+  const html = fs.readFileSync('./index.html', 'utf8')
+  res.end(html);
 });
 
 server.listen(8088)
