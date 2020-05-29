7b92ca353025d645263c4b527dd280bbcb522cc9
xuyiming <xuyimingwork@foxmail.com>
2020-05-29 11:20:26 +0800

    fix(server): reset Content-Type

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ const fs = require('fs');
 const server = http.createServer((req, res) => {
   console.log(`${req.method} ${req.url} HTTP/${req.httpVersion}`);
   console.log(req.headers, '\n');
-  res.setHeader('Content-Type', 'text/html');
   res.setHeader('X-Foo', 'bar');
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.writeHead(200, { 'Content-Type': 'text/html' });
   const html = fs.readFileSync('./index.html', 'utf8')
   res.end(html);
 });
