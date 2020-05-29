e98904403ba994d861e678201fb084c7c7e528c7
xuyiming <xuyimingwork@foxmail.com>
2020-05-29 11:52:07 +0800

    feat(parser): setup FSM frame

--- parser.js
+++ parser.js
@@ -1,3 +1,14 @@
+const EOF = Symbol('EOF') // End Of File
+
+function data(c) {
+  console.log(JSON.stringify(c))
+  return data
+}
+
 module.exports.parseHTML = function(html) {
-  console.log(html)
+  let state = data
+  for (let c of html) {
+    state = state(c)
+  }
+  state = state(EOF)
 }
